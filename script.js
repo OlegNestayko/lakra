34b7fb8a7ec30cdd954cd5a8a82b87c436974133
OlegNestayko <oleg_nestayko@ukr.net>
2022-09-18 23:52:00 +0300

    Added html marcup,styeles css,js code for spoiler.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Slick slider
+
 $('.header-slider').slick();
+
+// Spoiler
+
+$(document).ready(function () {
+  $('.item-units__spoiler').click(function (event) {
+    if ($('.units__list').hasClass('one')) {
+      $('.item-units__spoiler').not($(this)).removeClass('active');
+      $('.item-units__desc').not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
+});
